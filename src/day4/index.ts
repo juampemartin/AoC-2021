24c6c50be74f66865e3c19b872d3e044564952d5
juampemartin <juamp_m@hotmail.com>
2021-12-11 00:06:32 +0100

    Finished day 5

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -5,65 +5,96 @@ const content = fs
   .readFileSync(path.join(__dirname, 'input.prod'), {
     encoding: 'utf-8',
   })
+  .toString();
+
+let guesses: number[] = content
+  .trim()
+  .split('\n')[0]
+  .trim()
+  .split(',')
+  .map(guess => parseInt(guess));
+
+type Board = number[][];
+
+// Boards declaration
+let boards: Board[] = content
+  .trim()
+  .split('\n')
+  .splice(2)
+  .join('\n')
   .split('\n\n')
-  .filter(x => Boolean(x))
-  .map(x =>
-    x
-      .replace(/[\n ,]+/g, ' ')
-      .trim()
-      .split(' ')
-      .map(y => parseInt(y)),
-  );
+  .map(board => {
+    return board.split('\n').map(row =>
+      row
+        .trim()
+        .split(/\s+/)
+        .map(value => parseInt(value)),
+    );
+  });
 
-let [drawnNumbers, ...cards] = content;
+// Function to check if the board has won
+const boardHasWinner = (board: Board): boolean => {
+  let hasWinner = false;
 
-class Card {
-  numbers: Array<Number>;
-  lines: Array<number>;
-  columns: Array<number>;
-  cardSize: number;
-  numberToPosition: any;
-  isComplete: boolean;
-  markedNumbers: any;
-
-  constructor(numbers) {
-    this.cardSize = 5;
-    this.numbers = numbers;
-    this.numberToPosition = new Map();
-    for (let i = 0; i < this.numbers.length; i++) {
-      const n = this.numbers[i];
-      this.numberToPosition.set(n, {
-        line: Math.floor(i / this.cardSize),
-        column: i % this.cardSize,
-      });
+  board.forEach(row => {
+    if (row.filter(value => value === -1).length === row.length) {
+      hasWinner = true;
     }
-    this.lines = Array(this.cardSize).fill(0);
-    this.columns = Array(this.cardSize).fill(0);
-    this.isComplete = false;
-    this.markedNumbers = new Set();
-  }
+  });
 
-  addMarkedNumber(number: Array<number>) {
-    const position = this.numberToPosition.get(number);
-    if (!position) {
-      return;
-    }
-    this.markedNumbers.add(number);
-    this.lines[position.line]++;
-    this.columns[position.column]++;
+  for (let i = 0; i < board[0].length; i++) {
+    let columnValues: number[] = [];
+    board.forEach(row => {
+      columnValues.push(row[i]);
+    });
     if (
-      this.lines[position.line] === this.cardSize ||
-      this.columns[position.column] === this.cardSize
+      columnValues.filter(value => value === -1).length === columnValues.length
     ) {
-      this.isComplete = true;
+      hasWinner = true;
     }
   }
 
-  unmarkedNumbers() {
-    return this.numbers.filter(n => !this.markedNumbers.has(n));
-  }
+  return hasWinner;
+};
 
-  showCard() {
-    const array = [];
-  }
-}
+// Calculate Score Function
+const calculateScore = (board: Board, guess: number): number => {
+  let sum = 0;
+  board.forEach(row => {
+    row.forEach(value => {
+      if (value > -1) {
+        sum += value;
+      }
+    });
+  });
+  return sum * guess;
+};
+
+const main = () => {
+  let winningBoards: number[] = [];
+
+  guesses.forEach(guess => {
+    boards.forEach((board, boardIndex) => {
+      board.forEach((row, rowIndex) => {
+        row.forEach((value, valueIndex) => {
+          if (value === guess) {
+            row[valueIndex] = -1;
+          }
+        });
+      });
+
+      if (boardHasWinner(board) && winningBoards.indexOf(boardIndex) === -1) {
+        let score = calculateScore(board, guess);
+        winningBoards.push(boardIndex);
+
+        if (winningBoards.length === 1) {
+          console.log('Part One: ', score);
+        } else if (winningBoards.length === boards.length) {
+          console.log('Part Two: ', score);
+        }
+      }
+    });
+  });
+};
+
+main();
